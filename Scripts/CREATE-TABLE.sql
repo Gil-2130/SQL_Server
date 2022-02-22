68603b27b5a03782d7a7ef1a9d6ff60ac12ed17d
Giliard <gil2614@live.com>
2022-02-22 20:59:00 -0300

    Add files via upload

---
+++ Scripts/CREATE-TABLE.sql
@@ -0,0 +1,17 @@
+/* CREATE TABLE
+Aprendendo a criar tabelas
+Sintaxe;
+CREATE TABLE NomeTabela (
+	coluna1 tipo RestricaoDaColuna,
+	coluna2 tipo RestricaoDaColuna,
+	coluna3 tipo RestricaoDaColuna,
+   ....
+);
+===== PRINCIPAIS TIPOS DE RESTRIÇÕES QUE PODEM SER APLICADAS =====
+* NOT NULL --> Não Permite Nulos
+* UNIQUE --> Força que todos os valores em uma coluna sejam diferentes
+* PRIMARY KEY --> Uma junção de NOT NULL e UNIQUE
+* FOREIGN KEY --> Identificação de uma unica linha em outra tabela
+* CHECK --> Força uma condição especifica em uma coluna.
+* DEFAULT --> Força um valor padrão quando nenhum valor é passado
+*/
