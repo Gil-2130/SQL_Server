e89ff11cb7d80ec4964f6c46d96d090812c1cfe3
Giliard P. Santos <gil2614@live.com>
2022-03-17 19:42:53 -0300

    Add files via upload

---
+++ Scripts/SELECT-e-WHERE.sql
@@ -0,0 +1,47 @@
+/*
+Operador      -     Descrição
+=					IGUAL
+>					MAIOR QUE
+<					MENOR QUE
+<>					DIFERENTE
+>=					MAIOR OU IGUAL
+<=					MENOR OU IGUAL
+AND					OPERADOR LOGICO 'E'
+OR					OPERADOR LOGICO 'OU'
+*/
+
+SELECT *
+FROM Person.Person
+WHERE LastName = 'miller' and FirstName = 'anna'
+
+SELECT *
+FROM Production.Product
+WHERE Color= 'blue' or Color = 'black'
+
+SELECT *
+FROM Production.Product
+WHERE ListPrice > 1500
+
+SELECT *
+FROM Production.Product
+WHERE ListPrice > 1500 and ListPrice < 5000
+
+SELECT * 
+FROM Production.Product
+WHERE color <> 'red'
+
+SELECT * 
+FROM Production.Product
+WHERE Weight >= 500 and Weight <= 700
+
+SELECT *
+FROM HumanResources.Employee
+WHERE MaritalStatus = 'M' and SalariedFlag = 1
+
+SELECT *
+FROM Person.Person, Person.EmailAddress
+WHERE FirstName = 'Peter' and LastName = 'Krebs'
+
+SELECT *
+FROM Person.EmailAddress
+WHERE BusinessEntityID = 26
