6b5a47dce432cf5c4ddb9eb35a5c992cf2fbf9db
Giliard P. Santos <gil2614@live.com>
2022-03-25 22:03:32 -0300

    Add files via upload

---
+++ Scripts/SELECT-e-AS-renomeando.sql
@@ -0,0 +1,28 @@
+/* Aula complementar de "as" --> Usado para renomear alguma coluna retornada de alguma operação
+*/
+
+-- Exemplo 1 sem nome
+SELECT TOP 10 *
+FROM Production.Product
+
+-- Exemplo 2 - Selecionando coluna e atribuindo um nome
+SELECT TOP 10 ListPrice as "Lista de Preços"
+FROM Production.Product
+
+-- Exemplo 3 - Selecionando coluna
+SELECT TOP 10 avg(listPrice) as "Preço Médio"
+FROM Production.Product
+
+-- DESAFIO 1 -> Encontre o FirstName e LastName de person.person e renomeie
+SELECT FirstName as "Primeiro Nome", LastName as "Último Nome"
+FROM Person.Person
+
+-- DESAFIO 2 -> Encontre e renomeie a coluna ProductNumber da tabela Production.product
+
+SELECT ProductNumber as "Número de Produto"
+FROM Production.Product
+
+-- DESAFIO3 -> 
+
+SELECt UnitPrice as "Preço Unitário"
+FROM Sales.SalesOrderDetail
