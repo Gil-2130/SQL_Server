aae84e876cd6aba7eb0c33ad7fd42fe3f581316a
Giliard P. Santos <gil2614@live.com>
2022-03-26 21:35:06 -0300

    Add files via upload

---
+++ Scripts/SELECT-e-SUM-MAX-MIN-AVG.sql
@@ -0,0 +1,17 @@
+SELECT TOP 10 *
+FROM Sales.SalesOrderDetail
+
+-- Obtendo a soma de uma determinada coluna
+SELECT TOP 10 sum(LineTotal) --as soma *
+FROM Sales.SalesOrderDetail
+
+-- Obs; Ao retornar o resultado, uma coluna é apresentada sem nome.
+-- Você Pode dar um nome temporário a ela apenas para tornar os resultados de facil compreensão
+
+-- Obtendo a média e nomeando a coluna resultante
+SELECT TOP 10 AVG(LineTotal) as Média
+FROM Sales.SalesOrderDetail
+
+--Obtendo o mínimo e nomeando a coluna
+SELECT TOP 10 MIN(LineTotal) as Mínimo
+FROM Sales.SalesOrderDetail
