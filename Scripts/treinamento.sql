fc5b8646258c82ac9171cd00fdbe4404c3d06829
Giliard P. Santos <gil2614@live.com>
2022-04-02 20:13:22 -0300

    Add files via upload

---
+++ Scripts/treinamento.sql
@@ -0,0 +1,43 @@
+SELECT *
+FROM Production.Product
+
+SELECT TOP (500) [ProductID],
+				 [Name],
+				 [ProductNumber],
+				 [Color],
+				 [ListPrice]
+FROM [AdventureWorks2017].[Production].[Product]
+ORDER BY [Name] DESC
+
+-- Criando Stored Procedure para este banco de dados
+CREATE PROCEDURE TesteDB
+AS
+BEGIN
+	SELECT TOP (500) [ProductID],
+					 [Name],
+					 [ProductNumber],
+					 [Color],
+					 [ListPrice]
+	FROM [AdventureWorks2017].[Production].[Product]
+	ORDER BY [Name] ASC
+END
+
+-- Verificando se a Procedure foi criada
+EXECUTE TesteDB
+
+
+-- Alterando a Procedure
+ALTER PROCEDURE TesteDB
+AS
+BEGIN
+	SELECT TOP (500) [ProductID],
+					 [Name],
+					 [ProductNumber],
+					 [Color],
+					 [ListPrice]
+	FROM [AdventureWorks2017].[Production].[Product]
+	ORDER BY [Name] DESC
+END
+
+-- VErificando as novas alterações na procedure
+EXECUTE TesteDB
