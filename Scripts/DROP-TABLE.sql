c3a92bfe677e1ecc8f2709ee11a35087a58c54b3
Giliard P. Santos <gil2614@live.com>
2022-04-07 20:52:45 -0300

    Usando o DROP TABLE

--- Scripts/DROP-TABLE.sql
+++ Scripts/DROP-TABLE.sql
@@ -1,14 +1,16 @@
 /*
+
 DROP TABLE --> Vamos aprender a dropar(excluir) uma tabela do banco de dados 
 Usando DROP TABLE
 --===== SINTAXE =====
 DROP TABLE NomeTabela
 
-Obs; N„o È possÌvel realizar o DROP em tabelas referenciadoras ou seja;
-Tabelas que s„o referenciadas por outras tabelas (FOREIGN KEY CONSTRAINT), Tabelas que possuem chave estrangeira 
+Obs; N√£o √© poss√≠vel realizar o DROP em tabelas referenciadoras ou seja;
+Tabelas que s√£o referenciadas por outras tabelas (FOREIGN KEY CONSTRAINT), Tabelas que possuem chave estrangeira 
+
 */
 
--- Iremos dropar uma tabela que n„o È referenciadora
+-- Iremos dropar uma tabela que n√£o √© referenciadora
 DROP TABLE ErrorLog
 
 -- Verificando se a tabela foi realmente excluida;
@@ -19,7 +21,7 @@ SELECT errorlog
 -- ===== EXCLIUNDO APENAS O CONTEUDO DA TABELA =====
 TRUNCATE TABLE Person.Password
 
--- Verificando se as informaÁıes da tabela foram excluÌdas
+-- Verificando se as informa√ß√µes da tabela foram exclu√≠das
 SELECT *
 FROM Person.Password
 
