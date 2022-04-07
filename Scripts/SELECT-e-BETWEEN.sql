530516a45e825b7fc64b45ade24c20db3ce8ec52
Giliard P. Santos <gil2614@live.com>
2022-04-07 20:54:34 -0300

    SELECT e BETWEEN

--- Scripts/SELECT-e-BETWEEN.sql
+++ Scripts/SELECT-e-BETWEEN.sql
@@ -1,5 +1,6 @@
-/* SQL Between
-… usado para encontrar um valor mÌnimo e um valor m·ximo
+/*
+SQL Between
+√â usado para encontrar um valor m√≠nimo e um valor m√°ximo
 */
 
 -- Realizando consulta e obtendo valores dentro de um range
@@ -12,7 +13,7 @@ SELECT *
 FROM Production.Product
 WHERE ListPrice  not between 1000 and 1500
 
--- Consultando a tabela de Recursos humanos e buscando por funcion·rios em um range de datas
+-- Consultando a tabela de Recursos humanos e buscando por funcion√°rios em um range de datas
 SELECT * 
 FROM HumanResources.Employee
 WHERE HireDate between '2009/01/01' and '2010/12/31'
