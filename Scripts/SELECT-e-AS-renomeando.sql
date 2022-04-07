0078e2b8235cb90d54ee16fa3cc7ef8d1bb6a96a
Giliard P. Santos <gil2614@live.com>
2022-04-07 20:53:58 -0300

    RENOMEANDO uma instrução

--- Scripts/SELECT-e-AS-renomeando.sql
+++ Scripts/SELECT-e-AS-renomeando.sql
@@ -1,4 +1,5 @@
-/* Aula complementar de "as" --> Usado para renomear alguma coluna retornada de alguma operaÁ„o
+/* Aula complementar de "as" --> Usado para renomear alguma coluna retornada de alguma opera√ß√£o
+
 */
 
 -- Exemplo 1 sem nome
@@ -6,23 +7,23 @@ SELECT TOP 10 *
 FROM Production.Product
 
 -- Exemplo 2 - Selecionando coluna e atribuindo um nome
-SELECT TOP 10 ListPrice as "Lista de PreÁos"
+SELECT TOP 10 ListPrice as "Lista de Pre√ßos"
 FROM Production.Product
 
 -- Exemplo 3 - Selecionando coluna
-SELECT TOP 10 avg(listPrice) as "PreÁo MÈdio"
+SELECT TOP 10 avg(listPrice) as "Pre√ßo M√©dio"
 FROM Production.Product
 
 -- DESAFIO 1 -> Encontre o FirstName e LastName de person.person e renomeie
-SELECT FirstName as "Primeiro Nome", LastName as "⁄ltimo Nome"
+SELECT FirstName as "Primeiro Nome", LastName as "√öltimo Nome"
 FROM Person.Person
 
 -- DESAFIO 2 -> Encontre e renomeie a coluna ProductNumber da tabela Production.product
 
-SELECT ProductNumber as "N˙mero de Produto"
+SELECT ProductNumber as "N√∫mero de Produto"
 FROM Production.Product
 
 -- DESAFIO3 -> 
 
-SELECt UnitPrice as "PreÁo Unit·rio"
+SELECt UnitPrice as "Pre√ßo Unit√°rio"
 FROM Sales.SalesOrderDetail
