46e77d251c473198547fc902c5cac526606ca260
Giliard P. Santos <gil2614@live.com>
2022-04-10 20:40:23 -0300

    SELECT + IN

--- Scripts/SELECT-e-IN.sql
+++ Scripts/SELECT-e-IN.sql
@@ -1,4 +1,5 @@
-/* IN -> usamos o opeador IN juntamente com WHERE, para verificar se um valor
+/*
+IN -> usamos o opeador IN juntamente com WHERE, para verificar se um valor
 corresponde com qualquer valor passado na lista de valores.
 */
 
@@ -6,14 +7,14 @@ SELECT *
 FROM Person.Person
 WHERE BusinessEntityID IN (2, 7, 13)
 
--- a Query acima é mais produtiva do que;
+-- a Query acima Ã© mais produtiva do que;
 SELECT *
 FROM Person.Person
 WHERE BusinessEntityID = 2
 OR BusinessEntityID = 7
 OR BusinessEntityID = 13
 
--- Condição negativa
+-- CondiÃ§Ã£o negativa
 SELECT * 
 FROM Person.Person
 WHERE BusinessEntityID NOT IN (2, 7, 13)
