85834c6c720b5651fd17855ea7f98a488b38928a
Giliard P. Santos <gil2614@live.com>
2022-04-10 20:41:32 -0300

    SELECT + LIKE

--- Scripts/SELECT-e-LIKE.sql
+++ Scripts/SELECT-e-LIKE.sql
@@ -1,19 +1,21 @@
-/* SQL, Comando LIKE -> Suponhamos que vocÍ queira encontrar uma pessoa no banco de dados,
-e sponhamos que vocÍ saiba apenas as inicias do nome e deja saber qual o nome completo dela tipo ant...
+/*
+SQL, Comando LIKE -> Suponhamos que voc√™ queira encontrar uma pessoa no banco de dados,
+e sponhamos que voc√™ saiba apenas as inicias do nome e deja saber qual o nome completo dela tipo ant...
 
-para buscar todas as relaÁıes com as inicias do nome dela, vocÍ usaria os comandos abaixo;
+para buscar todas as rela√ß√µes com as inicias do nome dela, voc√™ usaria os comandos abaixo;
 
 SELECT *
 FROM tabela.tabela (por exemplo)
-WHERE FirstName like 'ant%' -> o percentual que n„o importa o complemento da palavra, mas os argumentos que foram passados
+WHERE FirstName like 'ant%' -> o percentual que n√£o importa o complemento da palavra, mas os argumentos que foram passados
+
 */
 
--- Exemplo 1 - par‚metros no inicio
+-- Exemplo 1 - par√¢metros no inicio
 SELECT *
 FROM Person.Person
 WHERE FirstName like 'ant%'
 
--- Exemplo 2 - par‚metros no final
+-- Exemplo 2 - par√¢metros no final
 SELECT *
 FROM Person.Person
 WHERE FirstName like '%nio'
@@ -23,7 +25,7 @@ SELECT *
 FROM Person.Person
 WHERE FirstName like '%ton%'
 
--- Exemplo 4 - buscando combinaÁıes no final mas com apenasuma letra depois dos par‚metros
+-- Exemplo 4 - buscando combina√ß√µes no final mas com apenasuma letra depois dos par√¢metros
 SELECT *
 FROM Person.Person
 WHERE FirstName like '%ni_' -- Usa-se o underline para especiicar quantos caracteres queremos antes ou depois dos argumentos
