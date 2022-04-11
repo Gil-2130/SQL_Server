ca426ca920be64aaccfd442267025ab86d642411
Giliard P. Santos <gil2614@live.com>
2022-04-11 20:57:15 -0300

    SELECT + SUM + MIN + MAX + AVG

--- Scripts/SELECT-e-SUM-MAX-MIN-AVG.sql
+++ Scripts/SELECT-e-SUM-MAX-MIN-AVG.sql
@@ -1,3 +1,7 @@
+/*
+Fun√ß√µes matem√°ticas
+*/
+
 SELECT TOP 10 *
 FROM Sales.SalesOrderDetail
 
@@ -5,13 +9,13 @@ FROM Sales.SalesOrderDetail
 SELECT TOP 10 sum(LineTotal) --as soma *
 FROM Sales.SalesOrderDetail
 
--- Obs; Ao retornar o resultado, uma coluna È apresentada sem nome.
--- VocÍ Pode dar um nome tempor·rio a ela apenas para tornar os resultados de facil compreens„o
+-- Obs; Ao retornar o resultado, uma coluna √© apresentada sem nome.
+-- Voc√™ Pode dar um nome tempor√°rio a ela apenas para tornar os resultados de facil compreens√£o
 
--- Obtendo a mÈdia e nomeando a coluna resultante
-SELECT TOP 10 AVG(LineTotal) as MÈdia
+-- Obtendo a m√©dia e nomeando a coluna resultante
+SELECT TOP 10 AVG(LineTotal) as M√©dia
 FROM Sales.SalesOrderDetail
 
---Obtendo o mÌnimo e nomeando a coluna
-SELECT TOP 10 MIN(LineTotal) as MÌnimo
+--Obtendo o m√≠nimo e nomeando a coluna
+SELECT TOP 10 MIN(LineTotal) as M√≠nimo
 FROM Sales.SalesOrderDetail
