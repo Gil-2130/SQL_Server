c91bd1b6f45c169bc1f5f86d2aa335cd735db7e7
Giliard P. Santos <gil2614@live.com>
2022-04-14 20:13:38 -0300

    CREATE + VIEW

--- Scripts/CREATE-VIEW.sql
+++ Scripts/CREATE-VIEW.sql
@@ -1,11 +1,12 @@
 /*
+
 VIEWS --> Basicamente as Views são tabelas criadas para consultas,
 onde você usa outras tabelas como base para criar uma nova tabela de pesquisa,
 com apenas dados específicos que você precisa de uma determinada tabela.
 
 É uma forma de ver apenas algumas informações sem precisar imprimir toda a tabela.
 
---===== SYNTAXE =====
+--========== SYNTAXE =========
 CREATE VIEW [Pessoas Simplificado] AS qualquer nome
 SELECT FirstName, MiddleName, LastName
 FROM Person.Person
